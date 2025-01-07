c7a4ab3d98af1cc19c3d63c7da9355ad987d8f58
Minemetero <minemetero@outlook.com>
2025-01-07 13:23:58 +0900

    fix: update SVG path fill color to white for improved visibility in toggle button

--- Ignitiaplus.js
+++ Ignitiaplus.js
@@ -574,7 +574,7 @@
         toggleButton.style.alignItems = "center";
         toggleButton.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24" height="24" style="display: block; margin: auto;">
-          <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"></path>
+          <path fill="white" d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"></path>
         </svg>`;
         
         toggleButton.addEventListener('click', () => {
