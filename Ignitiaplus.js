a941244d0ba3eb972a1fffbb036132a901f4cedc
Minemetero <minemetero@outlook.com>
2025-03-10 11:04:41 +0900

    feat: Add web worker to keep website alive

--- Ignitiaplus.js
+++ Ignitiaplus.js
@@ -337,6 +337,26 @@
         }
     }
 
+    function keepWebsiteAlive() {
+        const workerCode = `
+        self.onmessage = function(e) {
+            if (e.data === 'start') {
+                setInterval(() => {
+                self.postMessage('tick');
+                }, 1200000);
+            }
+        };
+        `;
+        const blob = new Blob([workerCode], { type: 'application/javascript' });
+        const worker = new Worker(URL.createObjectURL(blob));
+
+        worker.onmessage = function (e) {
+            console.log('Worker says:', e.data);
+        };
+
+        worker.postMessage('start');
+    }
+
     /*** Page Modifiers ***/
     function modifyPageHead() {
         const titleElement = document.querySelector('title');
@@ -456,10 +476,8 @@
         });
 
         contributorLink.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 -960 960 960"
-                 style="width: 24px; height: 24px; display: block; margin: 0 auto;">
-              <path d="M680-119q-8 0-16-2t-15-7l-120-70q-14-8-21.5-21.5T500-249v-141q0-16 7.5-29.5T529-441l120-70q7-5 15-7t16-2q8 0 15.5 2.5T710-511l120 70q14 8 22 21.5t8 29.5v141q0 16-8 29.5T830-198l-120 70q-7 4-14.5 6.5T680-119ZM400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm320-400q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Zm174-166 94 55 94-55-94-54-94 54Zm124 208 90-52v-110l-90 53v109Zm-150-52 90 53v-109l-90-53v109Z"></path>
+            <svg xmlns="http://www.w3.org/2000/svg"viewBox="0 -960 960 960"style="width: 24px; height: 24px; display: block; margin: 0 auto;">
+                <path d="M680-119q-8 0-16-2t-15-7l-120-70q-14-8-21.5-21.5T500-249v-141q0-16 7.5-29.5T529-441l120-70q7-5 15-7t16-2q8 0 15.5 2.5T710-511l120 70q14 8 22 21.5t8 29.5v141q0 16-8 29.5T830-198l-120 70q-7 4-14.5 6.5T680-119ZM400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm320-400q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Zm174-166 94 55 94-55-94-54-94 54Zm124 208 90-52v-110l-90 53v109Zm-150-52 90 53v-109l-90-53v109Z"></path>
             </svg>`;
 
         contributorTab.appendChild(contributorLink);
@@ -722,10 +740,10 @@
 
             function updateTimer() {
                 if (!running) return;
-                
+
                 const currentTime = Date.now();
                 elapsedTime = (currentTime - startTime) / 1000; // in seconds
-                
+
                 const hours = Math.floor(elapsedTime / 3600);
                 const minutes = Math.floor((elapsedTime % 3600) / 60);
                 const seconds = (elapsedTime % 60).toFixed(2);
@@ -762,7 +780,7 @@
                 localStorage.setItem('stopwatchStartTime', startTime.toString());
                 document.addEventListener('visibilitychange', handleVisibilityChange);
             }
-            
+
             function handleVisibilityChange() {
                 if (document.visibilityState === 'visible') {
                     const storedStartTime = parseInt(localStorage.getItem('stopwatchStartTime') || '0', 10);
@@ -1287,6 +1305,7 @@
     /*** Initialization ***/
     async function init() {
         injectCSS();
+        //keepWebsiteAlive();
         if (window.location.pathname.startsWith('/owsoo/login/auth')) {
             if (JSON.parse(localStorage.getItem('quoteWidget') || 'true')) addInspirationalQuoteWidget();
             removeLoginError();
