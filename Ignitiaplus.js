b83568292ced76214a74451bf0af8839e772e310
Minemetero <minemetero@outlook.com>
2025-04-04 21:50:28 +0900

    chore: Bump IgnitiaPlus version to 2.0.2

--- Ignitiaplus.js
+++ Ignitiaplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IgnitiaPlus
 // @namespace    http://tampermonkey.net/
-// @version      2.0.1
+// @version      2.0.2
 // @license      Apache-2.0
 // @description  Enhance your study experience with IgnitiaPlus
 // @author       Minemetero
